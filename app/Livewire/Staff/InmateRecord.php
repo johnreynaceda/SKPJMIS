5c7b2abd50def6d180b78879c10d0927afe8110b
Johnrey Naceda <75570397+johnreynaceda@users.noreply.github.com>
2025-02-06 09:38:50 +0800

    update

--- app/Livewire/Staff/InmateRecord.php
+++ app/Livewire/Staff/InmateRecord.php
@@ -1,15 +1,10 @@
 <?php
-
 namespace App\Livewire\Staff;
 
 use App\Models\Inmate;
-use App\Models\InmateFingerprint;
-use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Fieldset;
 use Filament\Forms\Components\FileUpload;
-use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Section;
-use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\ViewField;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
@@ -32,17 +27,17 @@ class InmateRecord extends Component implements HasForms, HasTable
     {
         return $table
             ->query(Inmate::query())->headerActions([
-                Action::make('new')->label('New Inmates')->icon('heroicon-o-user-plus')->color('main')->url(fn (): string => route('staff.inmate-create'))
-            ])
+            Action::make('new')->label('New Inmates')->icon('heroicon-o-user-plus')->color('main')->url(fn(): string => route('staff.inmate-create')),
+        ])
             ->columns([
                 TextColumn::make('created_at')->date()->label('CREATED DATE')->searchable(),
                 TextColumn::make('fullname')->label('NAME')->searchable(),
                 TextColumn::make('status')->label('STATUS')->badge()->searchable()->formatStateUsing(
                     fn($record) => ucfirst($record->status)
-                )->color(fn (string $state): string => match ($state) {
-                    'pending' => 'warning',
-                    'approved' => 'success',
-                    'dismissed' => 'danger',
+                )->color(fn(string $state): string => match ($state) {
+                    'pending'                          => 'warning',
+                    'approved'                         => 'success',
+                    'dismissed'                        => 'danger',
                 }),
             ])
             ->filters([
@@ -50,37 +45,37 @@ class InmateRecord extends Component implements HasForms, HasTable
             ])
             ->actions([
                 ActionGroup::make([
-                    
-                    Action::make('view')->color('warning')->icon('heroicon-o-eye'),
+
+                    // Action::make('view')->color('warning')->icon('heroicon-o-eye'),
                     Action::make('view_fingerprint')->label('View Fingerprint')->icon('heroicon-c-finger-print')->color('success')->form([
                         Section::make('FINGERPRINTS')->schema([
                             Fieldset::make('RIGHT HAND')->schema([
                                 ViewField::make('right_hand')
-                                 ->view('filament.forms.right-finger')
+                                    ->view('filament.forms.right-finger'),
                             ])->columns(1),
                             Fieldset::make('LEFT HAND')->schema([
                                 ViewField::make('left_hand')
-                                ->view('filament.forms.left-finger')
+                                    ->view('filament.forms.left-finger'),
                             ])->columns(1),
-                        ])
+                        ]),
                     ])->modalHeading('')->modalWidth('7xl')->modalSubmitAction(false),
                     Action::make('decriptive_record')->label('Descriptive Record')->icon('heroicon-o-document-text')->form([
                         Section::make('DESCRIPTIVE RECORD')
-                        ->description('This descriptive Record form will be used for all prisoners confined in a provincial prison and a copy of same certified as true and correct will accompany all prisoners upon their transfer from a provincial prison in addition to the commitment required by Executive Order No. 55 of 1997. In the identification record scars, marks and moles as well as the designation of missing members and deformities or peculiarities with dimension is millimeters will be recorded and located on the figure. Special care will be taken in lining and valuing prisoners effects and in securing their verifications to effect as listed and valued.')
-                        ->schema([
-                            FileUpload::make('front'),
-                            FileUpload::make('back'),
-                        ])->columns(2)
+                            ->description('This descriptive Record form will be used for all prisoners confined in a provincial prison and a copy of same certified as true and correct will accompany all prisoners upon their transfer from a provincial prison in addition to the commitment required by Executive Order No. 55 of 1997. In the identification record scars, marks and moles as well as the designation of missing members and deformities or peculiarities with dimension is millimeters will be recorded and located on the figure. Special care will be taken in lining and valuing prisoners effects and in securing their verifications to effect as listed and valued.')
+                            ->schema([
+                                FileUpload::make('front'),
+                                FileUpload::make('back'),
+                            ])->columns(2),
                     ])->modalWidth('6xl'),
 
                     DeleteAction::make('delete'),
-                ])
+                ]),
             ])
             ->bulkActions([
                 // ...
             ])->emptyStateHeading('No Inmates yet')->emptyStateDescription('Once you write your first inmate, it will appear here.');
     }
-    
+
     public function render()
     {
         return view('livewire.staff.inmate-record');
